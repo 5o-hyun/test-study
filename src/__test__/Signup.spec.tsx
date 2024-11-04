378a011764f29eca9a93776b55d8a3cf35f6b081
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-11-04 20:18:09 +0900

    03.Jest성공케이스작성

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -9,9 +9,7 @@ const queryClient = new QueryClient({
 });
 
 describe("회원가입 테스트", () => {
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다.", async () => {
-    // 1. given 준비단계 - 회원가입 페이지가 그려짐
-    // 회원가입 페이지는 react-router-dom(useNavigate)나 react-query등을 사용하고있기때문에 provider로 감싸줘야한다.
+  beforeEach(() => {
     const routes = [{ path: "/signup", element: <SignupPage /> }];
 
     const router = createMemoryRouter(routes, {
@@ -24,6 +22,10 @@ describe("회원가입 테스트", () => {
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
+  });
+
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다.", async () => {
+    // 1. given 준비단계 - 회원가입 페이지가 그려짐 => beforeEach로 대체
 
     // 2. when 실행단계 - 비밀번호롸 비밀번호 확인 값이 일치하지 않음
     // 라벨값으로 input을 가져옴
@@ -40,5 +42,29 @@ describe("회원가입 테스트", () => {
     // id가 error-message인 값을 찾아 존재하는지 확인, 에러메세지는 기다렸다가 실패시 띄워야하기때문에 await
     const errorMessage = await screen.findByTestId("error-message");
     expect(errorMessage).toBeInTheDocument();
+  });
+
+  test("이메일을 입력하고, 비밀번호와 비밀번호 확인값이 일치하면 회원가입 버튼이 활성화된다", () => {
+    // 1. given : 회원가입페이지가 그려짐 위와동일. => beforeEach로 대체
+
+    // 현재 버튼이 비활성화 되어있는지 테스트
+    const signupButton = screen.getByRole("button", { name: "회원가입" });
+    expect(signupButton).toBeDisabled();
+    // 2. when : 이메일 입력, 비밀번호 비밀번호확인 일치
+    const emailInput = screen.getByLabelText("이메일");
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인");
+
+    // 테스트케이스 성공을위해, password와 wrongPassword에 같은 값을 넣어줌
+    fireEvent.change(emailInput, {
+      target: { value: "button-active@gmail.com" },
+    });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: "password" },
+    });
+
+    // 3. then : 회원가입 버튼 활성화
+    expect(signupButton).toBeEnabled();
   });
 });
