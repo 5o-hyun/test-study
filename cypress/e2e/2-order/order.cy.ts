ca805f51108ba25fe9a4064a9b9fdf73fb210374
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-11-07 22:09:59 +0900

    04.Cypress 공용 컴포넌트 테스트

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -1,5 +1,14 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    cy.visit("/");
+
+    // 버튼이 존재하는지 검사
+    // cy.get("[data-cy=deliveryBtn]").should("exist").as("deliveryBtn");
+    cy.get("[data-cy=deliveryBtn]").should("be.visible").as("deliveryBtn");
+    cy.get("[data-cy=pickupBtn]").should("be.visible").as("pickupBtn");
+
+    // 존재하면, 클릭하고 food-type페이지로이동
+    cy.get("@deliveryBtn").click();
+    cy.url().should("include", "/food-type");
   });
 });
